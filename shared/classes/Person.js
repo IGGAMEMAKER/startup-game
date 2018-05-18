c3f3e893a15b7440d1c2cb92def35b883a59f233
IGGAMEMAKER <23i03g@mail.ru>
2018-05-18 14:24:25 +0300

    es6 to backend!

--- shared/classes/Person.js
+++ shared/classes/Person.js
@@ -1,9 +1,9 @@
 import nameGenerator from '../utils/create-person-name';
 
 export default class Person {
-  constructor({
+  constructor(
     name
-  }) {
+  ) {
     this.name = name ? name : nameGenerator();
   }
 }
